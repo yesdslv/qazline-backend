8d135e6c68a25dc9e2de5fe6f879fe1cd7dbef85
yesdslv <yesdslv@gmail.com>
2021-01-27 00:04:48 +0600

    Moved defining task type
    -Added define task type to model save method
    -Removed defining task type from signals

--- django_backend/src/qazline/signals.py
+++ django_backend/src/qazline/signals.py
@@ -1,12 +1,9 @@
 import os
 
-from django.db.models.signals import post_delete, pre_save
+from django.db.models.signals import post_delete
 from django.dispatch import receiver
-from django.core.validators import ValidationError
 
-from qazline.models import VideoMaterial, ImageMaterial, AssignmentMaterial, QuizMaterial, Image, Task
-
-FILL_THE_BLANK_SPECIAL_CHARS = '_____'
+from qazline.models import VideoMaterial, ImageMaterial, AssignmentMaterial, QuizMaterial, Image
 
 
 def _delete_file(path):
@@ -17,50 +14,9 @@ def _delete_file(path):
 
 @receiver(post_delete, sender=Image)
 def delete_file(sender, instance, *args, **kwargs):
+    # TODO Check if this condition is needed
     if instance.image:
         _delete_file(instance.image.path)
-
-
-def _define_task_type(task: Task):
-    answers = task.answers
-    question = task.question
-    n_true_answer = 0
-    n_values = 0
-    for answer in answers:
-        if 'correct' in answer:
-            n_values += 1
-            is_correct = answer['correct']
-            if is_correct:
-                n_true_answer += 1
-    _is_valid_answers(answers, question, n_values)
-    if n_values == 0:
-        task_type = Task.TaskType.FILL_IN_THE_BLANK
-    elif n_true_answer == 1:
-        task_type = Task.TaskType.SINGLE_ANSWER
-    else:
-        task_type = Task.TaskType.MULTIPLE_ANSWERS
-    return task_type
-
-
-def _is_valid_answers(answers, question, n_values):
-    if n_values != 0:
-        # Check single choice and multipe choice question
-        if len(answers) != n_values:
-            raise ValidationError({
-                'answers': 'In single-choice or multiple-choice tasks for not all answers are given correct values'
-            })
-    else:
-        n_blank_spec_chars = question.count(FILL_THE_BLANK_SPECIAL_CHARS)
-        if n_blank_spec_chars != len(answers):
-            raise ValidationError({
-                'answers': 'Number of answers are different from number of fill the blank special character'
-            })
-
-
-@receiver(pre_save, sender=Task)
-def set_task_type(sender, instance, *args, **kwargs):
-    task_type = _define_task_type(instance)
-    instance.task_type = task_type
 
 
 def delete_related_material(sender, instance, **kwargs):
